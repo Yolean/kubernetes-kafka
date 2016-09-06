920278f0271e8ac756637772e81c167c76e465a3
Staffan Olsson <staffan@repos.se>
2016-09-06 06:02:39 +0200

    Peer review revealed that some test commands were flawed. Only --old-producer producers would post what was piped.

--- test/test.sh
+++ test/test.sh
@@ -1,16 +1,22 @@
 
+# Run ls ./bin here to see the toolbox
+kubectl exec -ti testclient -- /bin/bash
+
+# List topics
+kubectl exec testclient -- ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --list
+
 # Create topic
 kubectl exec testclient -- ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --topic test1 --create --partitions 1 --replication-factor 1
 
 # Set one of your terminals to listen to messages on the test topic
 kubectl exec -ti testclient -- ./bin/kafka-console-consumer.sh --zookeeper zookeeper:2181 --topic test1 --from-beginning
 
-# Go ahead and produce topics, haven't found a way to do this directly through kubectl exec
-kubectl exec -ti testclient -- /bin/bash
-echo "Test $(date)" | ./bin/kafka-console-producer.sh --broker-list kafka-0.broker.kafka.svc.cluster.local:9092 --topic test1
-echo "Test $(date)" | ./bin/kafka-console-producer.sh --broker-list kafka-1.broker:9092,kafka-2.broker:9092 --topic test1
-# "WARN Removing server from bootstrap.servers as DNS resolution failed: kafka-X.broker:9092"
-echo "Test $(date)" | ./bin/kafka-console-producer.sh --broker-list kafka-0.broker:9092,kafka-1.broker:9092,kafka-2.broker:9092,kafka-X.broker:9092 --topic test1
+# Go ahead and produce messages
+echo "Write a message followed by enter, exit using Ctrl+C"
+kubectl exec -ti testclient -- ./bin/kafka-console-producer.sh --broker-list kafka-0.broker.kafka.svc.cluster.local:9092 --topic test1
+
+# Bootstrap even if two nodes are down (shorter name requires same namespace)
+kubectl exec -ti testclient -- ./bin/kafka-console-producer.sh --broker-list kafka-0.broker:9092,kafka-1.broker:9092,kafka-2.broker:9092 --topic test1
 
 # Topic 2, replicated
 ./bin/kafka-topics.sh --zookeeper zookeeper:2181 --describe --topic topic2
