9f47cd09eda070b7f6e1b7ef907431e239883146
Staffan Olsson <staffan@repos.se>
2017-07-23 07:02:13 +0200

    Upgrades to current https://github.com/solsson/dockerfiles/pull/5

--- update-kafka-image.sh
+++ update-kafka-image.sh
@@ -4,5 +4,5 @@ IMAGE=$1
 [ -z "$IMAGE" ] && echo "First argument should be the image to set" && exit 1
 
 for F in ./ test/ zookeeper/; do
-  sed -i '' "s|image: solsson/kafka:.*|image: $IMAGE|" $F*.yml
+  sed -i "s|image: solsson/kafka:.*|image: $IMAGE|" $F*.yml
 done
