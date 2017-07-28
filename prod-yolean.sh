98635cc4784b1ed64022343fb1731cf859b1f291
Staffan Olsson <staffan@repos.se>
2017-07-28 08:09:00 +0200

    Includes metrics in our prod

--- prod-yolean.sh
+++ prod-yolean.sh
@@ -6,7 +6,7 @@ git fetch
 git checkout origin/kafka-011
 git checkout -b prod-yolean-$(date +"%Y%m%dT%H%M%S")
 
-for BRANCH in addon-rest addon-kube-events-topic
+for BRANCH in addon-metrics addon-rest addon-kube-events-topic
 do
   git merge --no-ff $BRANCH -m "prod-yolean merge $BRANCH"
 done
