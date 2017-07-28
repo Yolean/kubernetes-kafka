c481cba64940778e6e279e7a018ae42ea69336e6
Staffan Olsson <staffan@repos.se>
2017-07-28 07:35:38 +0200

    This project avoids scripting through addons ...
    which means we need some scripting to define the production setup.

---
+++ prod-yolean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Combines addons into what we 'kubectl apply -f' to production
+set -ex
+
+git fetch
+git checkout origin/kafka-011
+git checkout -b prod-yolean-$(date +"%Y%m%dT%H%M%S")
+
+for BRANCH in addon-rest addon-kube-events-topic
+do
+  git merge --no-ff $BRANCH -m "prod-yolean merge $BRANCH"
+done
